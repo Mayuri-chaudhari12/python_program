ab503b00ae36ec3db0af35f1383ff2d7266e5e49
Mayuri-chaudhari12 <mayurichaudhari1601@gmail.com>
2025-02-04 09:25:27 +0530

    comment

---
+++ baisics/data_type.py
@@ -0,0 +1,44 @@
+#python datatypes
+
+# Binary Types:	bytes, bytearray, memoryview
+
+# Numeric Types:	int, float, complex
+a=10  
+b=10.20
+c=4+5j 
+print(type(a))
+print(type(b))
+print(type(c))
+print((c.real))
+print((c.imag))
+print((c.conjugate))
+
+# Text Type:	str
+name="mayuri"
+print(type(name))
+
+# Sequence Types:	list, tuple, range
+list=[10,20,30,40,50,50]
+tuple=(10,20,30,40,50,50)
+
+# Set Types:	set, frozenset
+set={10,20,30,40,50,50}
+frozenset=frozenset({10,20,30})
+
+# Mapping Type:	dict
+dictionary = {"10":"Python","20":"Java"}
+
+
+print(list)
+print(tuple)
+print(set)
+print(float)
+print(dictionary)
+
+# Boolean Type:	bool
+b = True
+print(b)
+
+# None Type:	NoneType
+k = None
+print(type(k))
